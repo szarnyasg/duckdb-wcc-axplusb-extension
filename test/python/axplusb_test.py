26e869fd4b9dc139efdbf08650c881b143cb6730
Gabor Szarnyas <szarnyasg@gmail.com>
2023-02-07 16:03:23 +0100

    Cleanup

--- test/python/axplusb_test.py
+++ test/python/axplusb_test.py
@@ -2,6 +2,6 @@ import duckdb
 
 def test_axplusb():
     conn = duckdb.connect('');
-    conn.execute('SELECT axplusb(1, 2, 3) as value;');
+    conn.execute('SELECT axplusb(1, 2, 3) as value;')
     res = conn.fetchall()
-    assert(res[0][0] == 1);
+    assert(res[0][0] == 1)
